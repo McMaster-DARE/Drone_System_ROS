352e64d5f0fe3843eba39b6c8e973b8fd6200946
DanYoung321 <youngd798@gmail.com>
2023-12-06 20:12:15 -0500

    updated temp_publisher to interface with TMP117

--- ydlidar_ws/src/canary_ros_driver/src/temp_publisher.py
+++ ydlidar_ws/src/canary_ros_driver/src/temp_publisher.py
@@ -1,19 +1,45 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg 
+import Float32
+import time
+import board
+import adafruit_tmp117
 
 def temp_publisher():
-    pub = rospy.Publisher('temp_topic', String, queue_size=10)
+    # Initialize the ROS publisher for the 'temp_topic' with Float32 messages
+    pub = rospy.Publisher('temp_topic', Float32, queue_size=10)
+    
+    # Initialize the ROS node as 'tmp117_publisher' and allow multiple nodes with the same name
     rospy.init_node('tmp117_publisher', anonymous=True)
+    
+    # Set the publishing rate to 1 Hz
     rate = rospy.Rate(1)
+    
+    # Log a message indicating that the publisher node has started
     rospy.loginfo("Publisher Node Started, now publishing messages")
+
+    # Initialize I2C communication and create a TMP117 sensor object
+    i2c = board.I2C()  # I2C interface using board.SCL and board.SDA
+    tmp117 = adafruit_tmp117.TMP117(i2c)
+
+    # Main loop for continuously publishing temperature readings
     while not rospy.is_shutdown():
-        msg = "Hello Emil - %s" % rospy.get_time()
-        pub.publish(msg)
+        # Read the temperature from the TMP117 sensor
+        temperature = tmp117.temperature
+        
+        # Log the temperature to the console for debugging
+        rospy.loginfo("Temperature: %.2f degrees C", temperature)
+        
+        # Publish only the numerical temperature value to the 'temp_topic'
+        pub.publish(temperature)
+
         rate.sleep()
 
 if __name__ == '__main__':
     try:
+        # Call the temperature publisher function
         temp_publisher()
     except rospy.ROSInterruptException:
+        # Handle a ROS interrupt exception 
         pass
