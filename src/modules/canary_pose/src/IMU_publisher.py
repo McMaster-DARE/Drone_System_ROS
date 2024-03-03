2bd3f37e7439295f556d2edbf4d8b5610710702a
DanYoung321 <youngd798@gmail.com>
2024-03-03 13:11:21 -0500

    Included covariance and vectorized values for  Imu topic

--- src/modules/canary_pose/src/IMU_publisher.py
+++ src/modules/canary_pose/src/IMU_publisher.py
@@ -6,9 +6,16 @@ import time
 import board
 import adafruit_bno055
 
+def create_vector3(p):
+    v = Vector3()
+    v.x = p[0]
+    v.y = p[1]
+    v.z = p[2]
+    
+    return v
 
 def IMU_publisher():
-    # Initialize the ROS publisher for the 'IMU_topic' with Float32 messages
+    # Initialize the ROS publisher for the 'IMU_topic' with Imu messages
     imu_pub = rospy.Publisher('/imu', Imu, queue_size=10)
     
     # Initialize the ROS node as 'IMU_publisher' and allow multiple nodes with the same name
@@ -25,28 +32,43 @@ def IMU_publisher():
     sensor = adafruit_bno055.BNO055_I2C(i2c)
     
     i = Imu()
+    i.orientation_covariance = [0.0, 0.0,0.0,
+    				 0.0, 0.0, 0.0,
+    				 0.0, 0.0, 0.0,]
+    i.angular_velocity_covariance = [0.0, 0.0,0.0,
+    				 0.0, 0.0, 0.0,
+    				 0.0, 0.0, 0.0,]
+    i.linear_acceleration_covariance = [0.0, 0.0,0.0,
+    				 0.0, 0.0, 0.0,
+    				 0.0, 0.0, 0.0,]
 
     # Main loop for continuously publishing readings
     while not rospy.is_shutdown():
 
         # Read the IMU values from the bno055 sensor 
         # can also get: temp, magnetic, euler, quaternion, linear accel  
+        q = sensor.quaternion
+        gy = sensor.gyro
+        acc = sensor.linear_acceleration
+        
+        
         i.header.stamp = rospy.Time.now()
         i.header.frame_id = ''
-        i.orientation = sensor.quaternion  
-        i.angular_velocity = sensor.gyro
-        i.linear_acceleration = sensor.linear_acceleration   
+        i.orientation = Quaternion(q[0], q[1], q[2], q[3])
+        i.angular_velocity = create_vector3(gy)
+        i.linear_acceleration = create_vector3(acc)
           
         # Log the temperature to the console for debugging                    
-        rospy.loginfo("Orientation: %s", i.orientation)
-        rospy.loginfo("Angular Velocity: %s", i.angular_velocity)
-        rospy.loginfo("Linear Acceleration: %s", i.linear_acceleration)
+        #rospy.loginfo("Orientation: %s", i.orientation)
+        #rospy.loginfo("Angular Velocity: %s", i.angular_velocity)
+        #rospy.loginfo("Linear Acceleration: %s", i.linear_acceleration)
              
 
         # Publish only the numerical temperature value to the 'IMU_topic'
-        
-        imu_pub.publish(i)
-
+        try:
+            imu_pub.publish(i)
+        except struct.error:
+            continue
         rate.sleep()
 
 if __name__ == '__main__':
